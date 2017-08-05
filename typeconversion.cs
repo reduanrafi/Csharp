35edc0f808a16405795c23a1591860bdedd99e51
reduanrafi <mr12129567@gmail.com>
2017-08-06 03:04:24 +0600

    type conversion file added

---
+++ typeconversion.cs
@@ -0,0 +1,21 @@
+using System;
+namespace Typeconversion{
+	class typeconversion{
+		static void Main(string[] args)
+		{
+			double d = 1.5;
+			int i;
+			
+			i = (int)d;
+			Console.WriteLine(i);
+			Console.ReadKey();
+			/* To run this program in command prompt 
+			   follw this instructions
+			   1.open command prompt as developer command prompt;
+			   2. goto to the program directory.
+			   3. type "csc programname.cs"
+			   4. again type the program name only wthout extension.
+			*/
+		}
+	}
+}
